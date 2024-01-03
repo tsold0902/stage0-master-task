9b07ff35ccaf5cbd212ef78187f143fa30d565db
tsold0902 <155520814+tsold0902@users.noreply.github.com>
2024-01-03 14:34:09 +0500

    Update HalfPyramid.java

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,9 +3,18 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
-    }
-
+		for(int i = cathetusLength; i >= 1; i-- )
+		{
+			for(int k = 1; k < i; k++)
+			{
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= cathetusLength - i; k++) {
+                System.out.print("*");
+            }
+			System.out.println();
+		}
+	}
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
     }
