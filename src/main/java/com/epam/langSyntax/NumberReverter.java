12e4d0c056be6f4d790f4e1081fd310d895f27f8
tsold0902 <155520814+tsold0902@users.noreply.github.com>
2024-01-03 14:33:16 +0500

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,22 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
-
-    public void revert(int number) {
-    }
+	public int revert(int number){
+		int rev = 0;
+		
+		while(number != 0) {
+			int rem = number % 10;
+			rev = rev * 10 + rem;
+			number /= 10;
+			}
+		return rev;
+	}
+	
+	public static void main(String[] args) {
+		NumberReverter myNum = new NumberReverter();
+		int number = 458;
+		int rev = myNum.revert(number);
+		System.out.println("Revert of " + number + " is: " + rev);
+	}
 
 }
