82159658ef712c2b4401b567e0a70f388eaf743a
tsold0902 <155520814+tsold0902@users.noreply.github.com>
2024-01-03 14:35:48 +0500

    Update Pyramid.java

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -2,8 +2,23 @@ package com.epam.loops;
 
 public class Pyramid {
 
-    public void printPyramid(int cathetusLength) {
+     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+        	
+            for (int j = cathetusLength - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
 
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
