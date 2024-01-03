5c2d1e8644548e44d8748ff1f88a9460cf1915a0
tsold0902 <155520814+tsold0902@users.noreply.github.com>
2024-01-03 14:34:52 +0500

    Update MultiplicationTable.java

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -2,7 +2,25 @@ package com.epam.loops;
 
 public class MultiplicationTable {
 
-    public void printTable(int numberTableToPrint) {
-    }
+	public void printTable(int numberTableToPrint) {
+		 if (numberTableToPrint <= 0) {
+	            System.out.println("Not Valid");
+	        } else {
+	            System.out.println("Multiplication table for " + numberTableToPrint + ":");
+
+	            for (int i = 1; i <= 10; i++) {
+	                System.out.println(numberTableToPrint + " x " + i + " = " + (numberTableToPrint * i));
+	            }
+	        }
+	    }
+	public static void main(String[] args) {
+		MultiplicationTable myTable = new MultiplicationTable();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a positive integer: ");
+		int number = input.nextInt();
+		
+		myTable.printTable(number);
+	}
 
 }
