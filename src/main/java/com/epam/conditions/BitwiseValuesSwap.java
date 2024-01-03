741be3a4c3d0b61676537a1e6477d434ca5eaaaa
tsold0902 <155520814+tsold0902@users.noreply.github.com>
2024-01-03 14:28:48 +0500

    Update BitwiseValuesSwap.java

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -1,8 +1,18 @@
 package com.epam.conditions;
 
 public class BitwiseValuesSwap {
-
-    public void swap(int first, int second) {
+	public void swap(int first, int second) {
+		System.out.println(first + " : " + second);
+		
+		first = first ^ second;
+		second = first ^ second; 
+		first = first ^ second;
+		
+		System.out.println(first + " : " + second);
     }
-
+	public static void main(String[] args) {
+		BitwiseValuesSwap mySwap = new BitwiseValuesSwap();
+		
+		mySwap.swap(2, 3);
+	}
 }
