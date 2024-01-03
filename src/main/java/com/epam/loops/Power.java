006fff8d37a7c7be758758cc86778088e4f15431
tsold0902 <155520814+tsold0902@users.noreply.github.com>
2024-01-03 14:35:19 +0500

    Update Power.java

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,8 +3,12 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
-
-    }
+		int result = 1;
+		for(int i = 0; i < power; i++) {
+			result = result * numberToPrint;
+		}
+		System.out.println(numberToPrint + " to the power of " + power + " is equal to: " + result);
+	}
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
