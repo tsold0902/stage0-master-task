88219974247e45036f70095c65c96c011866d686
tsold0902 <155520814+tsold0902@users.noreply.github.com>
2024-01-03 13:22:44 +0500

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,15 @@
 package com.epam.OOP;
 
-public class Bird {
-
+public class Bird extends Animal{
+	public Bird()
+	{
+		super("blue", 2, false);
+	}
+	
+	public String getDescription()
+	{
+		String animalDescription = super.getDescription();
+		return animalDescription + "Moreover, it has 2 wings and can fly.";
+	}
 }
+
